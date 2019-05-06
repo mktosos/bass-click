f3afb350e5f3cd19cc703934b994045cbba0fc38
mktosos <taniguchidesigns@hotmail.com>
2019-05-06 14:50:24 -0700

    added shuffle button

--- src/App.js
+++ src/App.js
@@ -16,6 +16,19 @@ class App extends React.Component {
     clickedImages: []
   };
 
+  handleShuffle = id => {
+    if (!this.state.clickedImages.includes(id)) {
+      this.setState({
+        score: this.state.score + 1,
+        clickedImages: [...this.state.clickedImages, id]
+      });
+    }
+    this.setState({
+      images: images.sort(() => Math.random() - 0.5),
+      totalClick: this.state.totalClick + 1
+    });
+  }
+
   
   
   render(){
@@ -28,6 +41,7 @@ class App extends React.Component {
       <h4>Score: {this.state.score}</h4>
       <h4>Top Score: {this.state.topScore}</h4>
       <h4>{this.state.guess}</h4>
+      <button onClick = {this.handleShuffle}>click to handleShuffle</button>
       
       
       <Wrapper>
@@ -41,9 +55,10 @@ class App extends React.Component {
       </Wrapper>
       </header>
       
+      
     </div>
   );
 }
 }
-console.log(images, "hi");
+
 export default App;
--- src/components/ImageCard/index.js
+++ src/components/ImageCard/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./style.css";
 
 
+
 function ImageCard(props) {
   return (
-    <div className="card" onClick={() => props.handleShuffle(props.id)} className="remove">
+    <div  onClick={() => props.handleShuffle(props.id)} className="remove">
       <div className="img-container">
         <img alt={"image id:" + props.id} src={props.image} />
       </div>
+      <span onClick={() => props.removeFriend(props.id)} className="remove">
+        𝘅
+      </span>
       
     </div>
   );
